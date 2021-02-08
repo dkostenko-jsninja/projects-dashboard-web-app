62f3edc201ca5810cebd087ec92cea9821819a3c
Dmytro Kostenko <ctvod1@gmail.com>
2021-02-08 15:06:34 +0200

    Initial commit

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { applyMiddleware, compose, createStore, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  combineReducers({}),
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
+
+export default store;
